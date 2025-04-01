35545fcc2da6c5756ab9b5ec3193e1caf6cf8d64
AartvB <aartvanbochove@hotmail.com>
2025-04-01 11:03:49 +0200

    Move deleted post to relevant table

--- delete_post.py
+++ delete_post.py
@@ -1,10 +1,22 @@
 import sqlite3
+import pandas as pd
 
 post_id = input("The post to delete has id: ")
 
 conn = sqlite3.connect("chicken_bot.db")
 cursor = conn.cursor()
+
+cursor.execute("SELECT * FROM chicken_posts WHERE id = ?", (post_id,))
+result = cursor.fetchone()
+
+cursor.execute('''
+    INSERT INTO deleted_posts (id, username, timestamp)
+    VALUES (?, ?, ?)
+''', (post_id, result[1], result[2]))
+conn.commit()
+
 query = "DELETE FROM chicken_posts WHERE id = ?"
 cursor.execute(query,(post_id,))
 conn.commit()
 conn.close()
+print(f"Post deleted succesfully: {result}")

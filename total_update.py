3882813901eae23f85900d22a3ccb4d9ba2f3921
AartvB <64839449+AartvB@users.noreply.github.com>
2025-08-23 09:52:21 +0200

    Do not send emails

--- total_update.py
+++ total_update.py
@@ -18,7 +18,7 @@ while True:
                 local_time = utc_time.astimezone(pytz.timezone('Europe/Amsterdam'))
                 time_first_error = local_time.strftime('%Y-%m-%d %H:%M:%S %Z')
         
-                cb.send_email('Stream error solved',f'{n_errors['stream']['n']-1} additional stream errors have happened, but an execution has started without any errors now, so the stream error has been solved. The first error occured at {time_first_error}. Latest error message:\n\n{n_errors['stream']['last_message']}')
+#                cb.send_email('Stream error solved',f'{n_errors['stream']['n']-1} additional stream errors have happened, but an execution has started without any errors now, so the stream error has been solved. The first error occured at {time_first_error}. Latest error message:\n\n{n_errors['stream']['last_message']}')
                 n_errors['stream']['first_error'] = 0
                 n_errors['stream']['n'] = 0
 
@@ -31,7 +31,7 @@ while True:
                         local_time = utc_time.astimezone(pytz.timezone('Europe/Amsterdam'))
                         time_first_error = local_time.strftime('%Y-%m-%d %H:%M:%S %Z')
 
-                        cb.send_email('Execution error solved',f'{n_errors['execution']['n']-1} additional execution errors have happened, but an execution has happened without any errors now, so the error has been solved. The first error occured at {time_first_error}. Latest error message:\n\n{n_errors['execution']['last_message']}')
+#                        cb.send_email('Execution error solved',f'{n_errors['execution']['n']-1} additional execution errors have happened, but an execution has happened without any errors now, so the error has been solved. The first error occured at {time_first_error}. Latest error message:\n\n{n_errors['execution']['last_message']}')
                         n_errors['execution']['first_error'] = 0
                         n_errors['execution']['n'] = 0
                     break
@@ -40,12 +40,12 @@ while True:
                     error_count = n_errors['execution']['n']
                     n_errors['execution']['last_message'] = e
                     if error_count == 0:
-                        cb.send_email('Execution error',f'An execution error occurred. Error message:\n{e}')
+#                        cb.send_email('Execution error',f'An execution error occurred. Error message:\n{e}')
                         n_errors['execution']['first_ever_error'] = time.time()
                         n_errors['execution']['first_error'] = time.time()
                         n_errors['execution']['n'] = 1
                     elif n_errors['execution']['first_error'] < time.time() - 5*60: # Send explanation email every 5 minutes
-                        cb.send_email('Execution error',f'Multiple {error_count} execution errors have occurred since last message, and they are still not solved. Latest error message:\n\n{e}')
+#                        cb.send_email('Execution error',f'Multiple {error_count} execution errors have occurred since last message, and they are still not solved. Latest error message:\n\n{e}')
                         n_errors['execution']['first_error'] = time.time()
                         n_errors['execution']['n'] = 1
                     else:
@@ -58,12 +58,12 @@ while True:
         error_count = n_errors['stream']['n']
         n_errors['stream']['last_message'] = e
         if error_count == 0:
-            cb.send_email('Stream error',f'A stream error occurred. Error message:\n{e}')
+#            cb.send_email('Stream error',f'A stream error occurred. Error message:\n{e}')
             n_errors['stream']['first_ever_error'] = time.time()
             n_errors['stream']['first_error'] = time.time()
             n_errors['stream']['n'] = 1
         elif n_errors['stream']['first_error'] < time.time() - 5*60: # Send explanation email every 5 minutes
-            cb.send_email('Stream error',f'Multiple {error_count} stream errors have occurred since last message, and they are still not solved. Latest error message:\n\n{e}')
+#            cb.send_email('Stream error',f'Multiple {error_count} stream errors have occurred since last message, and they are still not solved. Latest error message:\n\n{e}')
             n_errors['stream']['first_error'] = time.time()
             n_errors['stream']['n'] = 1
         else:
